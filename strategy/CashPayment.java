e00e2068af0c405d3389eccf3d31afbf8904fb49
toantientran1111 <toantran3104@gmail.com>
2026-06-11 23:05:30 +0700

    Toan hoan thanh code logic cho chuc nang Payment

--- strategy/CashPayment.java
+++
@@ -1,7 +0,0 @@
-package strategy;
-
-public class CashPayment implements PaymentStrategy {
-    public void pay(double amount) {
-        System.out.println("Thanh toán tiền mặt: $" + amount);
-    }
-}
--- strategy/VnPayPayment.java
+++
@@ -1,8 +0,0 @@
-package strategy;
-
-public class VnPayPayment implements PaymentStrategy {
-    @Override
-    public void pay(double amount) {
-        System.out.println("Thanh toán qua ví điện tử VNPAY: " + amount + " VND");
-    }
-}
